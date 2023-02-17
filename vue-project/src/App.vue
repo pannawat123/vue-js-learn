c114563073dbe229e46cca3c1e7091a5d72e07f8
kenjung <j.pannawat45@gmail.com>
2023-02-17 22:54:27 +0700

    add

---
+++ vue-project/src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import MyBar from './components/MyBar.vue';
+import MyCard from './components/MyCard.vue';
+
+</script>
+
+<template>
+  <header>
+    <MyBar />
+  </header>
+  <main>
+    <h1 style="padding-left: 10px;">MyCard</h1>
+    <div class="grid-container">
+      <MyCard v-for="item in items" v-bind="item" />
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+  created() {
+    fetch('https://www.melivecode.com/api/attractions')
+    .then(res => res.json())
+    .then((result) => {
+      this.items = result
+    })
+  }
+}
+</script>
+
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-column-gap: 0.5rem;
+  grid-row-gap: 0.5rem;
+}
+
+@media (min-width : 576px) {
+  .grid-container {grid-template-columns: repeat(1, 1fr);}
+}
+@media (min-width : 992px) {
+  .grid-container {grid-template-columns: repeat(3, 1fr);}
+}
+
+</style>
---
+++ vue-project/src/components/MyCard.vue
@@ -0,0 +1,28 @@
+<template lang="">
+   <div class="card">
+        <img :src="coverimage" :alt="name" style="width:100%">
+        <div class="container">
+        <h3><b>{{name}}</b></h3> 
+        <p>{{detail}}</p> 
+    </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['id', 'name' , 'detail', 'coverimage']
+}
+</script>
+<style scoped>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+</style>
